3137a7e6019397c8d0ddc8c857ec341945e459ff
Minicom667 <115790683+Minicom667@users.noreply.github.com>
2022-11-18 20:59:46 +0700

    Add files via upload

---
+++ encrypted.lua
@@ -0,0 +1,44 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("MIXTEN(Free)", "Synapse")
+
+
+local Tab = Window:NewTab("Blox Fruits")
+local Section = Tab:NewSection(" ")
+
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Minicom667/noob-autofarm/main/byme.lua'))()
+end)
+
+Section:NewButton("TAWANxHUB", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/kill55547/TAWAN_HUB/main/hub.lua.txt', true))()
+end)
+
+Section:NewButton("Neva Hub", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2"))()
+end)
+
+Section:NewButton("HoHo Hub", "ButtonInfo", function()
+    
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+end)
+
+Section:NewButton("KaiTun BF", "ButtonInfo", function()
+    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GooD1020/Update_kaitan_sazx_bata/main/SAZX.md"))()
+end)
+
+Section:NewButton("Yaretzi", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DeletedUserCode/Yaretzi/main/GameSupport.lua"))()
+end)
+
+local Tab = Window:NewTab("Other Games")
+local Section = Tab:NewSection("Build a boat")
+
+Section:NewButton("Rolly Hub 1", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XRoLLu/Rolly_Hub/main/open-source-trash-loader.exe.yeah"))()
+end)
+
+Section:NewButton("Rolly Hub 2", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/XRoLLu/UWU/main/BUILD%20A%20BOAT%20FOR%20TREASURE.lua'))()
+end)
