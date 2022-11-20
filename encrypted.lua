2dd4f45c89c937f1f2c1af277914ad4a6a9f10f2
Minicom667 <115790683+Minicom667@users.noreply.github.com>
2022-11-20 08:06:10 +0700

    Update encrypted.lua

--- encrypted.lua
+++ encrypted.lua
@@ -1,49 +1,42 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("MIXTEN(Free)", "DarkTheme")
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Patskorn/GUI/main/Copy-SynapOver.lua"))()
 
+local GUI = library:new("MixTen","Premium")
 
-local Tab = Window:NewTab("Blox Fruits")
-local Section = Tab:NewSection(" ")
+local Tab1 = GUI:Tap("BLOX FRUITS")
+local Tab2 = GUI:Tap("BUILD A BOAT")
 
-Section:NewButton("MIXTEN AUTO FARM", "ButtonInfo", function()
+Tab1:Button("MixTen Auto Farm(Not Good)",function()
     
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Minicom667/noob-autofarm/main/byme.lua'))()
 end)
 
-Section:NewButton("TAWANxHUB", "ButtonInfo", function()
+Tab1:Button("TawanXHub",function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/kill55547/TAWAN_HUB/main/hub.lua.txt', true))()
 end)
 
-Section:NewButton("Neva Hub", "ButtonInfo", function()
+Tab1:Button("Neva Hub",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2"))()
 end)
 
-Section:NewButton("HoHo Hub", "ButtonInfo", function()
+Tab1:Button("HoHo Hub",function()
     
 loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
 end)
 
-Section:NewButton("KaiTun BF", "ButtonInfo", function()
+Tab1:Button("Auto Farm speed",function()
     
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GooD1020/Update_kaitan_sazx_bata/main/SAZX.md"))()
 end)
 
-Section:NewButton("Yaretzi", "ButtonInfo", function()
+Tab1:Button("Yaretzi Hub",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DeletedUserCode/Yaretzi/main/GameSupport.lua"))()
 end)
 
-Section:NewButton("ButtonText", "MIXTEN PREMIUM(CR. xenon hub)", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Minicom667/MIXTEN-by-Teng-Z/main/NH.lua"))()
 
-end)
-
-local Tab = Window:NewTab("Other Games")
-local Section = Tab:NewSection("Build a boat")
-
-Section:NewButton("Rolly Hub 1", "ButtonInfo", function()
+Tab2:Button("Rolly Hub 1",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XRoLLu/Rolly_Hub/main/open-source-trash-loader.exe.yeah"))()
 end)
 
-Section:NewButton("Rolly Hub 2", "ButtonInfo", function()
+Tab2:Button("Rolly Hub 2",function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/XRoLLu/UWU/main/BUILD%20A%20BOAT%20FOR%20TREASURE.lua'))()
 end)
